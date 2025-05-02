5440a9a6ea388d2ef03148c8513ab3a274806925
Amoke Emmanuel <emmanuelamoke35@gmail.com>
2025-05-02 11:18:27 +0100

    version 0.7.3

--- src/app/(auth)/verify-user-email/page.jsx
+++ src/app/(auth)/verify-user-email/page.jsx
@@ -1,4 +1,7 @@
+"use client"
+
 import { VerifyUserEmail } from "@/components/root/veriy-user-email"
+import { Suspense } from "react";
 
 export default function VerifyUserEmailPage() {
     return (
