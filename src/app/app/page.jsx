11cf89532280976fe10b2c2a1e5cbc8b7f9ef064
Amoke Emmanuel <emmanuelamoke35@gmail.com>
2025-05-30 12:46:47 +0100

    version 0.9

--- src/app/app/page.jsx
+++ src/app/app/page.jsx
@@ -1,7 +1,12 @@
-"use client"
+import RepurposeUrl from "@/components/app/repurpose-url"
 
 export default function AppPage() {
     return (
-        <>emmsffdgdfgda</>
+        <div className="container mx-auto px-4 flex items-center justify-center min-h-[70vh]">
+            <div className="w-full">
+                <h1 className="text-green-800 text-4xl md:text-5xl lg:text-6xl mb-4 text-center font-extrabold">Welcome to Fiuzar</h1>
+                <RepurposeUrl />
+            </div>
+        </div>
     )
 }
---
+++ src/components/app/repurpose-url.jsx
@@ -0,0 +1,5 @@
+export default function RepurposeUrl() {
+    return (
+        <></>
+    )
+}
